cdd73fe3358aa3d88bb2484100772ba976f8a733
mitani1993 <fytim55@gmail.com>
2021-01-18 19:28:11 +0900

    正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+
   validates :birth, presence: true
+  validates :nickname, presence: true
+  
+
 end
