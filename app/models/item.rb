53931714d2490da4afea70231645994c0d2ec62f
mitani1993 <fytim55@gmail.com>
2021-01-19 14:42:28 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :content
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :shipping_area_id	
+    validates :shipping_date_id
+  end
+
+  validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
+
   belongs_to :user
 end
