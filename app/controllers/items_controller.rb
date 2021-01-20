3941c538edc508d7eda7cc845d2994db8cf87108
mitani1993 <fytim55@gmail.com>
2021-01-20 13:48:39 +0900

    updateアクション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,19 +19,20 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    if @item.save
-      redirect_to action: :index
-    else
-      render :new
-    end
   end
 
   def show
     @item = Item.find(params[:id])
   end
 
-  # def update
-  # end
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
 
   # def destroy
   # end
