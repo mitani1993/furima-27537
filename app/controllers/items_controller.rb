3e130dc2a24d19f3f28b0efc929318d0819e0824
mitani1993 <fytim55@gmail.com>
2021-01-20 14:45:55 +0900

    set_itemの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :destroy, :update]
+  before_action :set_item, except: [:index, :new, :destroy]
 
   def index
     @items = Item.includes(:user).order(id: 'DESC')
@@ -11,7 +12,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
     if @item.save
       redirect_to action: :index
     else
@@ -20,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to action: :show
     else
@@ -47,6 +44,10 @@ class ItemsController < ApplicationController
                                  :condition_id, :shipping_fee_id,
                                  :shipping_area_id, :shipping_date_id,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
