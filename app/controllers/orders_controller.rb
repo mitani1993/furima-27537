27387d9d19181e252459fc070630b76b62f75aef
mitani1993 <fytim55@gmail.com>
2021-01-21 11:38:09 +0900

    購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,15 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @user_order = UserOrder.new
   end
 
-  def new
+  def create
+    @user_order = UserOrder.new(order_params)
+  end
 
+  private
+  def order_params
+    params.require(:user_order).permit(:hoge, :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
   end
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -2,7 +2,7 @@ class UserOrder
   include ActiveModel::Model
   attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
 
-  with options presence: true do
+  with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :prefecture_id
     validates :city
