11f2fd1e7e8e7a6f02635aa80499aa21f36e2a69
mitani1993 <fytim55@gmail.com>
2021-01-21 16:47:08 +0900

    cardjs修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,9 +8,14 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
     @user_order = UserOrder.new(order_params)
     if @user_order.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: order_params[:price],
+        card: order_params[:token],
+        currency: 'jpy'           
+      )
       @user_order.save
       redirect_to root_path
     else
@@ -22,6 +27,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
+    binding.pry
     params.require(:user_order).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(
       user_id: current_user.id, item_id: params[:item_id, token: params[:token]]
     )
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_887bb80045728983ffe69960");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -21,10 +21,10 @@ const pay = () => {
         renderDom.insertAdjacentHTML("beforeend", tokenObj)
       }
 
-      document.getElementById("user_order_number").removeAttribute("name");
-      document.getElementById("user_order_exp_month").removeAttribute("name");
-      document.getElementById("user_order_exp_year").removeAttribute("name");
-      document.getElementById("user_order_cvc").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
