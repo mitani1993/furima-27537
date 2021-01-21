1983746caa3dc8e44fc210cadf3b876f96d06b95
mitani1993 <fytim55@gmail.com>
2021-01-21 10:47:31 +0900

    バリデーション設定

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,4 +1,14 @@
 class UserOrder
   include ActiveModel::Model
   attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+
+  with options presence: true do
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone_number
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 end
