162076bcfa1be4b4bacc13c25ea64083538ee575
mitani1993 <fytim55@gmail.com>
2021-01-21 11:13:28 +0900

    indexアクション記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
   resources :items do
-    resources :order only: :index
+    resources :orders, only: :index
   end
 end
