1456f606bf53f9a0f781ebdf213be211fd972a95
mitani1993 <fytim55@gmail.com>
2021-01-21 11:55:04 +0900

    createアクション記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,12 @@ class OrdersController < ApplicationController
 
   def create
     @user_order = UserOrder.new(order_params)
+    if @user_order.valid?
+      @user_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
